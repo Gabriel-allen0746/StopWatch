edd399488c3f9eb11557e7907ce7d9475c1cb544
gabriel0746 <gabriel-allen@hotmail.com>
2016-08-04 16:36:38 -0700

    Formatted time display with colons

--- StopWatch/ViewController.swift
+++ StopWatch/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var timer = NSTimer()
+    var counter = 0
     @IBOutlet weak var startButton: UIButton!
     @IBOutlet weak var stopButton: UIButton!
     @IBOutlet weak var timeLabel: UILabel!
@@ -17,19 +19,64 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stopButton.enabled = !stopButton.enabled
+        stopButton.hidden = !stopButton.hidden
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+
+    func altButtonState() {
+        startButton.hidden = !startButton.hidden
+        startButton.enabled = !startButton.enabled
+        
+        stopButton.hidden = !stopButton.hidden
+        stopButton.enabled = !stopButton.enabled
+    }
 
     @IBAction func stopTime(sender: AnyObject) {
-        timeLabel.text = "1:00"
+        timer.invalidate()
+        altButtonState()
+        counter = 0
     }
     
     @IBAction func startTime(sender: AnyObject) {
-        timeLabel.text = "0:00"
+        validateTimer()
+        altButtonState()
+    }
+    
+    func validateTimer() {
+        timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: #selector(ViewController.updateTime), userInfo: nil, repeats: true)
+    }
+    
+    func updateTime() {
+        counter += 1
+        
+        let (minutes, seconds, miliseconds) = calculateTimeUnits()
+        
+        timeLabel.text = "\(minutes):\(seconds):\(miliseconds)"
+    }
+    
+    func calculateTimeUnits() -> (String, String, String){
+        var remainingCounter = counter
+        
+        let minutes = remainingCounter / (60 * 100)
+        remainingCounter -= minutes * 60 * 100
+        
+        let seconds = remainingCounter / 100
+        remainingCounter -= seconds * 100
+        
+        let miliseconds = remainingCounter
+        
+        let stringMinutes = String(format: "%02d", minutes)
+        let stringSeconds = String(format: "%02d", seconds)
+        let stringMiliseconds = String(format:"%02d", miliseconds)
+        
+        return (stringMinutes, stringSeconds, stringMiliseconds)
     }
 
 }
